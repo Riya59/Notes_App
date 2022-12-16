76abbc9070155a0a84bc24f817e0236a49482d95
Riya Verma <77240974+Riya59@users.noreply.github.com>
2022-12-16 19:04:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 let items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
 app.set("view engine", "ejs");
@@ -45,6 +46,6 @@ app.post("/work", function(req,res){
 })
 
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("Server is running on port 3000");
 });
