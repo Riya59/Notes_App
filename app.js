337213f282ab37b554d88637747400d0f4c9f906
Riya Verma <77240974+Riya59@users.noreply.github.com>
2022-12-17 19:16:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get("/",function(req,res){
 
 app.post("/", function(req,res){
     let item = req.body.newItem;
-    if(req.body.list === "work"){
+    if(req.body.list === "Work"){
         workItems.push(item);
         res.redirect("/work");
     }
